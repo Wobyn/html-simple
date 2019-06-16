d963417d50065027c7e239bddbc8040e5f639e95
Wobyn <41243772+Wobyn@users.noreply.github.com>
2019-06-16 12:49:14 +1000

    Update global.js

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 var availableTags = [];
-var azureSearchQueryApiKey = "F7C69DB770C13A280301D20FE4586C0D";	// this is a query key for demo purposes
-var baseSearchURL = "https://azs-demos.search.windows.net/indexes/travel-stackexchange";
+var azureSearchQueryApiKey = "5D56792F3E60D4E1710894D659AB9BF3";	// this is a query key for demo purposes
+var baseSearchURL = "https://penelopesearchpage.azurewebsites.net/indexes/azureblob-index";
 
 var facetFiltersString = [];
 var facetFiltersCollection = [];
